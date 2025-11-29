cf8475f537f4a1e4ad3a9e64ce171b7e77bd6635
w7khan <w7khan@uwaterloo.ca>
2025-11-28 21:38:36 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -274,7 +274,7 @@ module player (
   wire w_shape;
   U_shape w1(pix_x, pix_y, W_X, y_pos, w_shape);
 
-  localparam [9:0] W_X2 = 10'd225;  // W_X + 8
+  localparam [9:0] W_X2 = 10'd226;  // W_X + 9
 
   // --- "W" shape ---
   wire w_shape2;
